e9aeda2eb426f85740a02c287f2211dccbf5d525
Jan Kowalski <yakov85@o2.pl>
2020-12-06 23:22:04 +0100

    Christmas Tree

---
+++ tree.rkt
@@ -0,0 +1,18 @@
+                         (
+                        map
+                       (λ(x)
+                      (let*([
+                     w display]
+                    [s(for([z(/(
+                   - 15 x)2)])(w
+                  " "))])(begin s
+                 (for([i x])(w"*")
+                )s(displayln""))))(
+                append(for/list([i(
+                in-range 1 16 2)])i
+                        )'(
+                        3 3
+                        )))
+                        
+                        
+                        https://ideone.com/t82MJt

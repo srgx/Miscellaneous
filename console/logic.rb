42201edc5b769e3ee0e26a50bee4d8f007c7049c
Jan Kowalski <yakov85@o2.pl>
2021-10-20 19:23:36 +0200

    Database

--- console/logic.rb
+++ console/logic.rb
@@ -3,6 +3,9 @@
 # Logic Programming
 
 # ---------------------------------------------------------
+SU = 'All tests passed!'
+FA = 'Some tests failed!'
+# ---------------------------------------------------------
 
 def rule?(fr)
   return 'r' == fr.first
@@ -12,58 +15,115 @@ def fact?(fr)
   return 'f' == fr.first
 end
 
-def askQuestion(q,d)
+def testSuccess(qs,db,fu)
+  puts method(fu).call(qs,db) ? SU : FA
+end
 
-  for i in 0...d.size do
+def runTests(qs,db)
+  testSuccess(qs,db,:askAllQuestions)
+end
 
-    fr = d[i]
+def runTestsAllFail(qs,db)
+  testSuccess(qs,db,:askAllQuestionsFail)
+end
 
+def testAll(qs,db,fu)
+  for i in 0...qs.size do
+    if method(fu).call(qs[i],db) then
+      return false
+    end
+  end
+  return true
+end
+
+def askAllQuestionsFail(qs,db)
+  return testAll(qs,db,:askQuestion)
+end
+
+def askAllQuestions(qs,db)
+  return testAll(qs,db,:askQuestionNot)
+end
+
+def askQuestion(qs,db)
+  for i in 0...db.size do
+    fr = db[i]
     if(fact?(fr))
-      if q == fr.drop(1) then
+      if qs == fr.drop(1) then
         return true
       end
     elsif(rule?(fr)) then
-      if q == fr[1] && askQuestion(fr[2],d) then
+      if qs == fr[1] && askAllQuestions(fr.drop(2),db) then
         return true
       end
     else
       raise 'Error'
     end
-
   end
-
   return false
+end
 
+def askQuestionNot(qs,db)
+  return !askQuestion(qs,db)
 end
 
 # ---------------------------------------------------------
 
-database =
-  [['f','happy','yolanda'],
+database_1 =
+  [['f','party'],
+   ['f','happy','yolanda'],
    ['f','listens','mia'],
-   ['r',['listens','yolanda'],['happy','yolanda']],
-   ['r',['plays','mia'],['listens','mia']],
-   ['r',['plays','yolanda'],['listens','yolanda']]]
+   ['r',['listens','yolanda'],
+        ['happy','yolanda']],
+   ['r',['plays','mia'],
+        ['listens','mia']],
+   ['r',['plays','yolanda'],
+        ['listens','yolanda']]]
 
-questions =
-  [['happy', 'yolanda'],
-   ['listens', 'mia'],
-   ['listens', 'yolanda'],
-   ['plays', 'mia'],
-   ['plays', 'yolanda']]
+questions_1 =
+  [['party'],
+   ['happy','yolanda'],
+   ['listens','mia'],
+   ['listens','yolanda'],
+   ['plays','mia'],
+   ['plays','yolanda']]
+
+fail_questions_1 =
+  [['concerto'],
+   ['happy','mia']]
 
 # ---------------------------------------------------------
 
-answers = Array.new(questions.size)
+database_2 =
+  [['f','happy','vincent'],
+   ['f','listens','butch'],
+   ['r',['plays','vincent'],
+        ['listens','vincent'],['happy','vincent']],
+   ['r',['plays','butch'],
+        ['happy','butch']],
+   ['r',['plays','butch'],
+        ['listens','butch']]]
 
-for i in 0...questions.size do
-  answers[i] = askQuestion(questions[i],database)
-end
+questions_2 =
+  [['happy', 'vincent'],
+   ['listens', 'butch'],
+   ['plays', 'butch']]
 
-if answers.all? then
-  puts 'All tests passed!'
-else
-  puts 'Some tests failed!'
-end
+fail_questions_2 =
+  [['plays', 'vincent'],
+   ['happy', 'butch']]
+
+# ---------------------------------------------------------
+
+print 'Set 1: '
+runTests(questions_1,database_1)
+
+print 'Set 2: '
+runTests(questions_2,database_2)
+
+print 'Set 3: '
+runTestsAllFail(fail_questions_1,database_1)
+
+print 'Set 4: '
+runTestsAllFail(fail_questions_2,database_2)
 
 # ---------------------------------------------------------
